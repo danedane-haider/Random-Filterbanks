f4701d65e9ac2fd9bc3ab742c221972aaa6b3a4a
Daniel Haider <55834940+danedane-haider@users.noreply.github.com>
2023-09-06 16:44:29 +1200

    Added the Synth Version of Fitting a CQT

--- Stability of Random Filterbanks under Gradient Descent/fb.py
+++ Stability of Random Filterbanks under Gradient Descent/fb.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 def frame_bounds_lp(w, freq=False):
     # if the filters are given already as frequency responses
     if freq:
-        w_hat = np.sum(np.abs(w)**2,axis=0)
+        w_hat = np.sum(np.abs(w)**2,axis=1)
     else:
         w_hat = np.sum(np.abs(np.fft.fft(w,axis=1))**2,axis=0)
     Lam_max = np.max(w_hat)
@@ -22,27 +22,45 @@ def frame_bounds_lp(w, freq=False):
 # creates a random filterbank of J filters of support T, padded with zeros to have length N
 # and optionally its tightened version
 
-def random_filterbank(N, J, T, tight=True, to_torch=True):
+def random_filterbank(N, J, T, norm=True, tight=True, to_torch=True, support_only=False):
     if T == None:
         T = N
-    # normalization
-    w = np.random.randn(J, T)/np.sqrt(T)/np.sqrt(J)
-    z = np.zeros([J, N-T])
-    w_cat = np.concatenate((w,z),axis=1)
-    if tight==False:
-        return torch.from_numpy(w_cat)
-    if tight==True:
-        # analysis operator
+    if norm:
+        w = np.random.randn(J, T)/np.sqrt(T)/np.sqrt(J)
+    else:
+        w = np.random.randn(J, T)
+    if support_only:
+        w_cat = w
+    else:
+        z = np.zeros([J, N-T])
+        w_cat = np.concatenate((w,z),axis=1)
+    if tight:
         W = np.concatenate([sp.linalg.circulant(w_cat[k, :]) for k in range(w_cat.shape[0])])
-        # frame operator
         S = np.matmul(W.T,W)
         S_sq = np.linalg.inv(sp.linalg.sqrtm(S))
         w_tight = np.matmul(S_sq,w_cat.T).T
+        if to_torch:
+            return torch.from_numpy(w_cat), torch.from_numpy(w_tight)
+        else:
+            return w_cat, w_tight
     if to_torch:
-        return torch.from_numpy(w_cat), torch.from_numpy(w_tight)
+        return torch.from_numpy(w_cat)
     else:
-        return w_cat, w_tight
+        return w_cat
+
     
+
+# diagonal of the frame operator of a filterbank with filters w
+
+def S_diag(w):
+    N = w.shape[1]
+    diag = torch.zeros(N)
+    for l in range(N):
+        rolled_w = torch.roll(w, shifts=l, dims=0)
+        squared_norms = torch.sum(rolled_w**2, dim=1)
+        diag[l] = torch.sum(squared_norms)
+
+    return diag
 
 
 # generate a dataset of random sine waves
@@ -59,7 +77,7 @@ def generate_random_sine(sample_rate, f_min, f_max, length, batch_size):
 
 # compute filterbank responses using conv1D (circulant)
 
-def filterbank_response(x, w):
+def filterbank_response(x, w, mag=True):
     # some shaping
     x = x.reshape(x.shape[0], 1, x.shape[-1])
     x = F.pad(x, (0, x.shape[-1]-1), mode='circular',)
@@ -68,8 +86,11 @@ def filterbank_response(x, w):
     # filtering
     out = F.conv1d(x, w, bias=None, stride=1, padding=0)
     # magnitude
-    mag = torch.abs(out)
-    return mag
+    if mag:
+        out = torch.abs(out)
+    return out
+
+
 
 
 # computes the circulant matrix version of w (tensor)
@@ -81,3 +102,22 @@ def circulant(w):
     tmp = torch.cat([w.flip((dim,)), torch.narrow(w.flip((dim,)), dim=dim, start=0, length=N-1)], dim=dim)
     tmp = tmp.unfold(dim, N, 1).flip((-1,))
     return tmp.reshape(J*N, N)
+
+
+
+
+def imp2freq(imp, mag=True, complex=False):
+    fr = np.fft.fft(imp,axis=1)#/np.sqrt(2)
+    if complex:
+        return fr.T
+    if mag:
+        return np.abs(fr.T)
+    return np.real(fr.T), np.imag(fr.T)
+
+def freq2imp(freq, mag=True, complex=False):
+    imp = np.fft.ifft(freq,axis=0)
+    if complex:
+        return imp.T
+    if mag:
+        return np.abs(imp.T)
+    return np.real(imp.T), np.imag(imp.T)
