81ef504daf63a554f6685f995fe36899cfb7fb9e
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2023-09-12 22:05:29 +0200

    f'/scratch/work/public/singularity/cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif'

--- Stability of Random Filterbanks under Gradient Descent/slurm_synth.py
+++ Stability of Random Filterbanks under Gradient Descent/slurm_synth.py
@@ -4,7 +4,7 @@ import slurmjobs
 jobs = slurmjobs.Singularity(
     "python slurm_synth.py",
     f'/scratch/{os.getenv("USER")}/ic24_overlay-15GB-500K.ext3',
-    "cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif",
+    f'/scratch/work/public/singularity/cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif',
     email='',
     sbatch=dict(time="12:00:00"),
     template="""{% extends 'job.singularity.j2' %}
