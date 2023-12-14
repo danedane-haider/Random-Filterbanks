0d3cf5bdaea0dc143a3155405528c026a0bf2984
Daniel Haider <55834940+danedane-haider@users.noreply.github.com>
2023-12-14 13:02:48 +0100

    tightener

--- Stability of Random Filterbanks under Gradient Descent/fb_utils.py
+++ Stability of Random Filterbanks under Gradient Descent/fb_utils.py
@@ -146,8 +146,19 @@ def circulant(w):
 
 # make tight
 
-def tight(w, ver='poly'):
-    if ver == 'poly':
+def tight(w, ver='S'):
+    """
+    Construction of the canonical tight filterbank
+    :param w: analysis filterbank
+    :param ver: version of the tight filterbank: 'S' yields canonical tight filterbank, 'flat_spec' yields tight filterbank with flat spectral response
+    :return: canonical tight filterbank
+    """
+    if ver == 'S':
+        w_freqz = np.fft.fft(w,axis=1)
+        lp = np.sum(np.abs(w_freqz)**2,axis=0)
+        w_freqz_tight = w_freqz * lp**(-0.5)
+        w_tight = np.fft.ifft(w_freqz_tight,axis=1)
+    elif ver == 'flat_spec':
         M, N = w.shape
         w_freqz = np.fft.fft(w,axis=1).T
         w_tight = np.zeros((M, N), dtype=np.complex64)
@@ -157,27 +168,28 @@ def tight(w, ver='poly'):
             w_tight[:,k] = np.conj(U)
         w_tight = np.fft.ifft(w_tight.T, axis=0).T
     else:
-        W = np.concatenate([sp.linalg.circulant(w[k, :]) for k in range(w.shape[0])])
-        S = np.matmul(W.T,W)
-        S_sq = np.linalg.inv(sp.linalg.sqrtm(S))
-        w_tight = np.matmul(S_sq,w.T).T
+        raise NotImplementedError
     return w_tight
 
 # make tight with fixed support
 
 def fir_tightener3000(w, supp, eps=1.1, print_kappa=False):
-    '''
-    input:  w - filterbank
-            supp - filter support
-            eps - threshold for kappa
-    '''
+    """
+    Iterative construction of a tight filterbank with a given support
+    :param w: analysis filterbank
+    :param supp: desired support of the tight filterbank
+    :param eps: desired precision for kappa = B/A
+    :return: tight filterbank
+    """
     A,B = frame_bounds_lp(w)
     w_tight = w.copy()
     while B/A > eps:
-        w_tight = tight(w_tight, ver='poly')
-        w_tight = np.real(w_tight)
+        w_tight = tight(w_tight)
         w_tight[:,supp:] = 0
+        w_tight = np.real(w_tight)
         A,B = frame_bounds_lp(w_tight)
+        kappa = B/A
+        error = np.linalg.norm(w-w_tight)
         if print_kappa:
-            print('kappa:', B/A, 'error:', np.linalg.norm(w-w_tight))
+            print('kappa:', '%.4f' % kappa, ', error:', '%.4f' % error)
     return w_tight
